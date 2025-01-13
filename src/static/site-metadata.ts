db399cd14c9720f6f9d93c9403bbb387ce4b4d4e
296015215 <296015215@qq.com>
2025-01-13 14:50:16 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,11 +16,7 @@ const data: ISiteMetadataResult = {
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
+      name: 'ThanksTo',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
     },
   ],
